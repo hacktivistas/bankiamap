ec9a7956bae5b187f32dcb0bb0b5a4262eed8e6d
Administrador del sistema <root@dev.democraciarealya.es>
2013-03-07 15:17:15 +0100

    actualizando index

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Language" CONTENT="en"/>
+		<meta name="author" CONTENT="Brankia S.A."/>
+		<meta name="copyitright" CONTENT="Brankia S.A., 2012. Rato. Todos los derechos en los reservados."/>
+		<meta name="title" CONTENT="Bankia"/>
+		<meta name="generator" CONTENT="Bankia"/>
+		<meta name="distribution" CONTENT="global"/>
+		<meta name="revisit" CONTENT="0 day" />
+		<meta name="robots" CONTENT="all" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link rel="stylesheet" href="estilos.css" />
+		<!--[if lte IE 8]> <link rel="stylesheet" href="estilos.ie.css" />  <![endif]-->  
+		<script type="text/javascript" src="lib/yepnope/yepnope.js"></script>
+		<title>Bankia</title>
+	</head>
+	<body>
+
+			<div id="cabecera">
+				<a href="index.php"><img src="images/TAB_logoWeb.png"></a>
+			</div>
+			<div id="cabecera2">
+				Operación Toque - Regístrate y participa - Sí se puede - El rescate - Accede a tu foro
+			</div>
+			<div id="cabecera3">
+				<p>
+					Registrate y participa
+				</p>
+
+				<table width="100%">
+					<tr>
+						<td width="20%"><img src="images/icon_geolocaliza.png"></td>
+						<td width="5%"><img src="images/flecha_proceso.png"></td>
+                                                <td width="20%"><img src="images/icon_oficina.png"></td>
+                                                <td width="5%"><img src="images/flecha_proceso.png"></td>
+                                                <td width="20%"><img src="images/icon_acceso.png"></td>
+                                            	<td width="5%"><img src="images/flecha_proceso.png"></td>
+                                                <td width="20%"><img src="images/icon_superapp.png"></td>
+					</tr>
+                                	<tr>
+                                                <td><span id="textc31">1. </span><span id="textc32">Pulsa en buscar y acepta geolocalizarte</span></td>
+                                                <td></td>
+                                                <td><span id="textc31">2. </span><span id="textc32">Escoge oficina</span></td>
+                                                <td></td>
+                                                <td><span id="textc31">3. </span><span id="textc32">Elige cómo entrar</span></td>
+                                                <td></td>
+                                                <td><span id="textc31">4. </span><span id="textc32">Acepta la App (sólo facebook)</span></td>
+                                        </tr>
+
+				</table>
+			</div>
+
+			<div id="contenedormapa1">
+				<div id="contenedormapa2">
+					<div id="mapa"></div>
+				</div>
+			</div>
+
+			<div id="cargando">Cargando...</div>
+			<div id="botonera"><button id="localizame" class="boton">Bankias cercanos</button></div>
+			
+			<script type="text/javascript" src="bankia.js"></script>
+
+	</body>
+</html>
