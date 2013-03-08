ea9ee90e374026f0e4dc91ba5124e00e0f96df9f
orangefanta <fanta@freesoftwareando.com>
2013-03-08 16:01:06 +0000

    Dir map creado. Allí esta el mapa separado para que joker no se enfade. En el index va en un iframe

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
 			<div id="contenedormapa1">
 				<div id="contenedormapa2">
-					<iframe src="http://hacktivistas.github.com/bankiamap/" width="100%" height="400"></iframe>						
+					<iframe src="map/mapa.html" width="100%" height="400"></iframe>						
 				</div>
 			</div>
 
