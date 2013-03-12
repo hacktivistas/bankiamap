5943098c66215ca975f6c27b8f8d46c1382ebf26
orangefanta <fanta@freesoftwareando.com>
2013-03-12 18:35:01 +0000

    subido mareo

--- operacion.php
+++ operacion.php
@@ -46,12 +46,24 @@
 			</div>
 			<div id="contenedorcuerpo2">
 				<div id="cuerpo2">
+					<table width="100%">
+						<tr>
+							<td width="60%">
 								<p id="textcuerpo1">Queremos interrumpir la normalidad de la oficina</p>
 								<p id="textcuerpo2">Interferir en la actividad habitual, en todas las sucursales, significa paralizar Bankia.</p>
 								<p id="textcuerpo1">Queremos ejercer derechos básicos de los clientes</p>
 								<p id="textcuerpo2">Vamos a saturar la actividad de la sucursal, con toda la potencia de nuestra imaginación. Desde preguntar exhaustivamente por todos los productos, hasta pasear al perro o sufrir una lipotimia en la oficina. La palabra clave es “Cansinismo”.</p>
 								<p id="textcuerpo2">Otra cuestión importante es tender al riesgo cero. Esto es bueno para que todo el mundo pueda participar en la acción</p>
+							</td>
+							<td>
+								<img src="images/TAB_quees_mareo.png">
+							</td>
+							
+						</tr>
+					</table>
+
 								<p id="textcuerpo1">Queremos hacerlo durante toda la jornada</p>
+								<center><img src="images/TAB_quees_horario.png"></center>
 								<p id="textcuerpo2">La eficacia está en la duración. Para bloquear toda la jornada laboral necesitamos ser suficientes para cubrir, por grupos, todas las franjas horarias. Aquí es clave evitar el desgaste excesivo de nadie.</p>
 								<p id="textcuerpo1">Queremos visibilizar el 99%</p>
 								<p id="textcuerpo2">El éxito de la acción es el éxito de todas las pequeñas acciones en el total de oficinas. La consigna aquí es ¡traete hasta a tu abuela! Por eso será importante que la información circule y se visibilice de la manera más coordinada posible.</p>
