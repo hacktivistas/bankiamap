1eadc424a7bbc00b7f04019a3beb1c853eab6b43
orangefanta <fanta@freesoftwareando.com>
2013-03-17 11:12:19 +0000

    rescate.php

---
+++ rescate.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Language" CONTENT="en"/>
+		<meta name="author" CONTENT="Brankia S.A."/>
+		<meta name="copyitright" CONTENT="Brankia S.A., 2012. Rato. Todos los derechos en los reservados."/>
+		<meta name="title" CONTENT="Bankia"/>
+		<meta name="generator" CONTENT="Bankia"/>
+		<meta name="distribution" CONTENT="global"/>
+		<meta name="revisit" CONTENT="0 day" />
+		<meta name="robots" CONTENT="all" />
+		<link rel="stylesheet" href="estilos.css" />
+		<meta property="og:title" content="Toque a Bankia" />
+		<meta property="og:type" content="cause" />
+		<meta property="og:url" content="http://bankia.mepone.net" />
+		<meta property="og:image" content="http://bankia.mepone.net/images/TAB_logoWeb.png" />
+		<meta property="og:site_name" content="Dale un toque a Bankia" />
+		<meta property="fb:admins" content="100003745894955" />
+		<!--[if lte IE 8]> <link rel="stylesheet" href="estilos.ie.css" />  <![endif]-->  
+		<title>Bankia - Toque a Bankia</title>
+	</head>
+	<body>
+	<div id="principal">
+			<div id="cabecera">
+				<a href="index.php"><img src="images/TAB_logoWeb.png"></a>
+			</div>
+			<div id="cabecera2">
+				<table width="100%">
+				<tr>
+					<td width="15%">&nbsp;</td>
+					<td width="14%"><a href="operacion.php"><span id="textc21">Operación Toque</span></a></td>
+					<td width="14%"><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></td>
+					<td width="14%"><span id="textc21">Sí se puede</span></td>
+					<td width="14%"><a href="rescate.php"><span id="textc21">El rescate</span></a></td>
+					<td width="14%"><span id="textc21">Accede a tu foro</span></td>
+					<td width="15%">&nbsp;</td>
+				</tr>
+				</table>
+			</div>
+			
+			<div id="presentacion">
+				<p id="textcuerpo1">¿Para qué hacemos "toque a Bankia"?</p>
+				<div class="non-semantic-protector">	
+				<h1 class="ribbon">
+					<strong class="ribbon-content"><span id="shit">Estas son nuestras exigencias</span></strong>
+				</h1>
+				</div>
+				
+			</div>
+			<div id="contenedorcuerpo2">
+				<div id="cuerpo2">
+
+<ol class="rectangle-list">
+	<li><a href=""><p id="textcuerpo1">Queremos hacerlo durante toda la jornada</p><p id="textcuerpo2">La eficacia está</p></a></li>
+	<li><a href="">List item</a></li>
+	<li><a href="">List item</a></li>						
+</ol>
+
+				</div>
+	
+				
+			</div>
+	</div>
+	</body>
+</html>
