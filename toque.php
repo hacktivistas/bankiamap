d0e5de0a80ba7c648782a49338202ea1d1278e2e
orangefanta <fanta@freesoftwareando.com>
2013-03-19 16:44:55 +0000

    re-ajustando index.php y toque.php con estilos nuevos de mar

--- toque.php
+++ toque.php
@@ -23,54 +23,40 @@
 	<body>
 	<div id="principal">
 			<div id="cabecera">
-				<a href="index.php"><img src="images/TAB_logoWeb.png"></a>
+				<a href="index.php"><img src="images/TAB_logoWeb.png"/></a>
 			</div>
 			<div id="cabecera2">
-				<table width="100%">
-				<tr>
-					<td width="15%">&nbsp;</td>
-					<td width="14%"><a href="operacion.php"><span id="textc21">Operación Toque</span></a></td>
-					<td width="14%"><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></td>
-					<td width="14%"><span id="textc21">Sí se puede</span></td>
-					<td width="14%"><a href="rescate.php"><span id="textc21">El rescate</span></a></td>
-					<td width="14%"><span id="textc21">Accede a tu foro</span></td>
-					<td width="15%">&nbsp;</td>
-				</tr>
-				</table>
-			</div>
+              <ul>
+                <li><a href="operacion.php"><span id="textc21">Operación Toque</span></a></li>
+                <li><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></li>
+                <li><span id="textc21">Sí se puede</span></li>
+                <li><a href="rescate.php"><span id="textc21">El rescate</span></a></li>
+                <li><span id="textc21">Accede a tu foro</span></td>
+			  </ul>
+            </div>
 			<div id="cabecera3">
-				<p>
-					<span id="textc30">Registrate y participa</span>
-				</p>
+				<h1>Reg&iacute;strate y participa</h1>
 
-				<table width="100%">
-					<tr>
-						<td width="6%">&nbsp;</td>
-						<td width="17%"><img src="images/icon_geolocaliza.png"></td>
-						<td width="5%"><img src="images/flecha_proceso.png"></td>
-						<td width="17%"><img src="images/icon_oficina.png"></td>
-						<td width="5%"><img src="images/flecha_proceso.png"></td>
-						<td width="17%"><img src="images/icon_acceso.png"></td>
-						<td width="5%"><img src="images/flecha_proceso.png"></td>
-						<td width="17%"><img src="images/icon_superapp.png"></td>
-						<td width="6%">&nbsp;</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td><span id="textc31">1 </span><span id="textc32">Pulsa en Buscar</span></td>
-						<td></td>
-						<td><span id="textc31">2 </span><span id="textc32">Escoge oficina</span></td>
-						<td></td>
-						<td><span id="textc31">3 </span><span id="textc32">Elige cómo entrar</span></td>
-						<td></td>
-						<td><span id="textc31">4 </span><span id="textc32">Aceptar App (sólo FB)</span></td>
-						<td></td>
-					</tr>
-				</table>
+				<ul id="proceso">
+                	<li><img src="images/icon_geolocaliza.png"/></li>
+                    <li><img src="images/flecha_proceso.png"/></li>
+                    <li><img src="images/icon_oficina.png"/></li>
+                    <li><img src="images/flecha_proceso.png"/></li>
+                    <li><img src="images/icon_acceso.png"/></li>
+                    <li><img src="images/flecha_proceso.png"/></li>
+                    <li><img src="images/icon_superapp.png"/></li>
+                </ul>
+                <ul id="procesotx">
+                	<li><ul><li class="num">1</li><li>Pulsa en buscar y<br/>acepta geolocalizarte</li></ul></li>
+                    <li><ul><li class="num">2</li><li>Escoge oficina</li></ul></li>
+                    <li><ul><li class="num">3</li><li>Elige c&oacute;mo entrar</li></ul></li>
+                    <li><ul><li class="num">4</li><li>Acepta la app<br/>(s&oacute;lo facebook)</li></ul></li>
+                </ul>
 				
-			</div>
+			</div><!--Fin cabecera 3-->
+            
 
-			<div id="contenedormapa1">
+			<div id="contenedormapa1" class="sombra">
 				<div id="contenedormapa2">
 					<iframe src="map/index.html" width="100%" height="400"></iframe>						
 				</div>
@@ -78,43 +64,34 @@
 
 
 			<div id="contenedorcuerpo">
-				<div id="cuerpo">
-					<table width="100%">
-						<tr>
-							<td valign="top" align="left"><img src="images/icon_bombilla.png"></td>
-							<td align="left" valign="top">&nbsp;<br><span id="textcuerpo1">Todo comienza por el mapa</span></td>
-							<td rowspan="2" valign="top" align="left"><img src="images/swarm_foros.png"></td>
-						</tr>
-						<tr>
-							<td valign="bottom" align="left"><img src="images/icon_ojo.png"></td>
-							<td valign="top" align="left">
-								<p id="textcuerpo2"><b>También puedes navegar por el mapa.</b> Si haces doble click, irás aproximándote al lugar donde quieras buscar tu oficina.</b>
-								<p id="textcuerpo2"><b>Cuando seleccionas una oficina</b> y te registras en ella, entras a formar parte de un grupo de acción, junto con las demás personas que la han escogido.</p>
-								<p id="textcuerpo2"><b>Cada grupo se comunica</b> a través de su foro. Sólo puedes pertenecer a un foro a la vez. De esta manera todas sabemos cuántas somos y podemos distribuir mejor las tareas.</p>
-								<p id="textcuerpo2"><b>Los grupos pueden ver</b> cómo lo están haciendo los demás y aprender de ellos, pero sólo participan en el foro de su sucursal.<p>
-								<p id="textcuerpo2">Por supuesto el teléfono y los bares siguen existiendo ;-)</p>
-							</td>
-							
-						</tr> 
-					</table>
-				</div>
+              <div class="uno cols"><img src="images/icon_bombilla.png"/></div>
+              <div class="siete cols"><h2>Todo comienza por el mapa</h2>
+                <p><strong>También puedes navegar por el mapa.</strong> Si haces doble click, irás aproximándote al lugar donde quieras buscar tu oficina.</b>
+                        <p><strong>Cuando seleccionas una oficina</strong> y te registras en ella, entras a formar parte de un grupo de acción, junto con las demás personas que la han escogido.</p>
+                        <p><strong>Cada grupo se comunica</strong> a través de su foro. Sólo puedes pertenecer a un foro a la vez. De esta manera todas sabemos cuántas somos y podemos distribuir mejor las tareas.</p>
+                        <p><strong>Los grupos pueden ver</strong> cómo lo están haciendo los demás y aprender de ellos, pero sólo participan en el foro de su sucursal.<p>
+                        <p>Por supuesto el teléfono y los bares siguen existiendo ;-)</p>
+                </div>
+                <div class="cuatro cols" style="text-align:center;"><img src="images/swarm_foros.png"/></div>
+                <div class="cortar"></div>
 			</div>	
+            
+            
 			<div id="contenedorinfo">
-				<div id="info">
-					<table cellpadding="20">
-						<tr>
-							<td align="left" valign="top">
-								<p id="textcuerpo2"><b>Si entras con tu usuario de Facebook</b> es más cómodo para tí y puedes interactuar también en ese espacio.  Además le das difusión a “Toque a Bankia”.</p>
-								<p id="textcuerpo2">La noticia de que te has apuntado a la acción aparecerá en tu muro y en el de todas las demás personas de tu grupo.</p>
-							</td>
-							<td align="left" valign="top">
-								<p id="textcuerpo2"><b>Si te registras con el formulario</b> todo es igual salvo que no has conectado tu Facebook a la acción y tu espacio privilegiado será el foro.</p>
-								<p id="textcuerpo2">En ambos casos es muy importante que el grupo se organice para definir formas de acción y disponibilidad horaria para la jornada.</p>
-							</td>
-						</tr>
-					</table>
+            
+				<div id="info" class="borde-dash">
+				 <div class="seis cols margeni15">
+                  <p><strong>Si entras con tu usuario de Facebook</strong> es más cómodo para tí y puedes interactuar también en ese espacio.  Además le das difusión a “Toque a Bankia”.</p>
+				  <p>La noticia de que te has apuntado a la acción aparecerá en tu muro y en el de todas las demás personas de tu grupo.</p>
+				 </div>
+				 <div class="seis cols margeni15">
+                 	<p><strong>Si te registras con el formulario</strong> todo es igual salvo que no has conectado tu Facebook a la acción y tu espacio privilegiado será el foro.</p>
+								<p>En ambos casos es muy importante que el grupo se organice para definir formas de acción y disponibilidad horaria para la jornada.</p>
+				 </div>
+                 <div class="cortar"></div>
 				</div>	
 			</div>
-	</div>
+	</div><!--Fin Principal-->
+    <div id="pie"> </div>
 	</body>
 </html>
