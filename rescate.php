49aa61c8d42fbc4bd3fe06924e86d5efca046099
Iván Eixarch <miao.org@gmail.com>
2013-03-21 02:49:45 +0100

    estilos Mar y nuevas páginas

--- rescate.php
+++ rescate.php
@@ -23,49 +23,41 @@
 	<body>
 	<div id="principal">
 			<div id="cabecera">
-				<a href="index.php"><img src="images/TAB_logoWeb.png"></a>
+				<a href="index.php"><img src="images/TAB_logoWeb.png"/></a>
 			</div>
 			<div id="cabecera2">
-				<table width="100%">
-				<tr>
-					<td width="15%">&nbsp;</td>
-					<td width="14%"><a href="operacion.php"><span id="textc21">Operación Toque</span></a></td>
-					<td width="14%"><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></td>
-					<td width="14%"><span id="textc21">Sí se puede</span></td>
-					<td width="14%"><a href="rescate.php"><span id="textc21">El rescate</span></a></td>
-					<td width="14%"><span id="textc21">Accede a tu foro</span></td>
-					<td width="15%">&nbsp;</td>
-				</tr>
-				</table>
+			  <ul>
+                <li><a href="operacion.php"><span id="textc21">Operación Toque</span></a></li>
+                <li><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></li>
+                <li><span id="textc21">Sí se puede</span></li>
+                <li><a href="rescate.php"><span id="textc21">El rescate</span></a></li>
+                <li><span id="textc21">Accede a tu foro</span></td>
+			  </ul>
 			</div>
 			
 			<div id="presentacion">
-				<p id="textcuerpo1">¿Para qué hacemos "toque a Bankia"?</p>
-				<div class="non-semantic-protector">	
-				<h1 class="ribbon">
-					<strong class="ribbon-content"><span id="shit">Estas son nuestras exigencias</span></strong>
-				</h1>
-
+				<h1>¿Para qué hacemos "toque a Bankia"?</h1>
+				<div id="orla">	
+					<h2 id="baner">Estas son nuestras exigencias</h2>
 				</div>
-
 			</div>
-			<div id="contenedorcuerpo3">
-				<div id="cuerpo3">
-					<ol>
-						<li><span id="textcuerpo1">Paralización total de los desahucios y dación en pago retroactiva</span><br><span id="textcuerpo2">o condonación de la deuda a las familias que han sufrido la ejecuación hipotecaria de su primera vivienda.</span></li>
-						<li><span id="textcuerpo1">Utilización para alquiler social de todas las viviendas de Bankia</span><br><span id="textcuerpo2">así como las que han pasado al Sareb (banco malo) provenientes de esta entidad.</span></li>
-						<li><span id="textcuerpo1">Recuperación de las participaciones preferentes</span><br><span id="textcuerpo2">conversión en cuentas corrientes de libre disposición, del ahorro puesto por las familias en participaciones preferentes.</span></li>
-						<li><span id="textcuerpo1">Auditoría ciudadana de la deuda de Bankia</span><br><span id="textcuerpo2">estableciendo mecanismos claros que permitan establecer y negarse al pago de la deuda odiosa siguiendo el modelo de juicio ciudadano. Publicación de la contabilidad de Bankia.</span></li>
-						<li><span id="textcuerpo1">Inversión directa en los presupuestos públicos de sanidad, educación e investigación no militar, de lo que se recupere con la auditoría de la deuda</span></li>
-						<li><span id="textcuerpo1">Nacionalización completa de Bankia</span><br><span id="textcuerpo2">pasando su control integro a manos del Estado y estableciendo mecanismos de transparencia, participación del 99% y democracia directa en su consejo de administración.</span></li>
-					</ol>
-					<hr>
+            
+			<div id="contenedorcuerpo3" class="sombra">
+               <ul id="listaexigencias">
+                <li><div class="num" >1</div><h3>Paralización total de los desahucios y dación en pago retroactiva</h3><p>o condonación de la deuda a las familias que han sufrido la ejecuación hipotecaria de su primera vivienda.</p></li>
+                <li><div class="num" >2</div><h3>Utilización para alquiler social de todas las viviendas de Bankia</h3><p>así como las que han pasado al Sareb (banco malo) provenientes de esta entidad.</p></li>
+                <li><div class="num" >3</div><h3>Recuperación de las participaciones preferentes</h3><p>conversión en cuentas corrientes de libre disposición, del ahorro puesto por las familias en participaciones preferentes.</p></li>
+                <li><div class="num" >4</div><h3>Auditoría ciudadana de la deuda de Bankia</h3><p>estableciendo mecanismos claros que permitan establecer y negarse al pago de la deuda odiosa siguiendo el modelo de juicio ciudadano. Publicación de la contabilidad de Bankia.</p></li>
+                <li><div class="num" >5</div><h3>Inversión directa en los presupuestos públicos de sanidad, educación e investigación no militar, de lo que se recupere con la auditoría de la deuda</h3><p></p></li>
+                <li><div class="num" >6</div><h3>Nacionalización completa de Bankia</h3><p>pasando su control integro a manos del Estado y estableciendo mecanismos de transparencia, participación del 99% y democracia directa en su consejo de administración.</p></li>
+            </ul>
+			<div class="hr"></div>
+            <div id="notapie">
 					<p id="textcuerpo2">Como habrá que entrar en detalles de cuáles son las maneras, los tiempos y el orden en que cada una de estas exigencias pueden desarrollarse, el establecimiento de una mesa abierta de diálogo y negociación puede considerarse suficiente para detener temporalmente Toque a Bankia.</p>
-					<p id="textcuerpo2"> Si las exigencias no son atendidas, si no podemos rescatar Bankia para las personas, la acción se repetirá en poco tiempo, y tanas veces como sea necesario hasta tumbarla. Para nosotras tumbar Bankia significa <b>forzar su intervención judicia</b>, que los especuladores pierdan su dinero y que las personas recuperemos el nuestro.</p>
-				</div>
-
-
+					<p id="textcuerpo2"> Si las exigencias no son atendidas, si no podemos rescatar Bankia para las personas, la acción se repetirá en poco tiempo, y tantas veces como sea necesario hasta tumbarla. Para nosotras tumbar Bankia significa <b>forzar su intervención judicial</b>, que los especuladores pierdan su dinero y que las personas recuperemos el nuestro.</p>
 			</div>
-	</div>
+		</div>
+	</div><!--Fin Principal-->
+    <div id="pie"> </div>
 	</body>
 </html>
