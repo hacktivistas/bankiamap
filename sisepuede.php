024d2b0262f8640bfa4d98d2894939f59eda6182
brankia <brankia@toqueabankia.net>
2013-04-07 04:52:49 +0200

    si se puede al repositorio

---
+++ sisepuede.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Language" CONTENT="en"/>
+		<meta name="author" CONTENT="Brankia S.A."/>
+		<meta name="copyitright" CONTENT="Brankia S.A., 2012. Rato. Todos los derechos en los reservados."/>
+		<meta name="title" CONTENT="Bankia"/>
+		<meta name="generator" CONTENT="Bankia"/>
+		<meta name="distribution" CONTENT="global"/>
+		<meta name="revisit" CONTENT="0 day" />
+		<meta name="robots" CONTENT="all" />
+		
+		<meta property="og:title" content="Toque a Bankia" />
+		<meta property="og:type" content="cause" />
+		<meta property="og:url" content="http://bankia.mepone.net" />
+		<meta property="og:image" content="http://bankia.mepone.net/images/TAB_logoWeb.png" />
+		<meta property="og:site_name" content="Dale un toque a Bankia" />
+		<meta property="fb:admins" content="100003745894955" />
+        
+        <link rel="stylesheet" href="estilos.css" />
+		<!--[if lte IE 8]> <link rel="stylesheet" href="estilos.ie.css" />  <![endif]-->  
+		<title>Bankia - Toque a Bankia</title>
+	</head>
+	<body>
+	<div id="principal">
+			<div id="cabecera">
+				<a href="index.php"><img src="images/TAB_logoWeb.png" /></a>
+			</div>
+			<div id="cabecera2">
+				<ul>
+					<li><a href="operacion.php"><span id="textc21">Operación Toque</span></a></li>
+					<li><a href="toque.php"><span id="textc21">Regístrate y participa</span></a></li>
+					<li><a href="sisepuede.php"><span id="textc21">Sí se puede</span></a></li>
+					<li><a href="rescate.php"><span id="textc21">El rescate</span></a></li>
+					<li><span id="textc21">Accede a tu foro</span></td>
+				</ul>
+			</div><!--Fin cabecera 2-->
+			
+            <div id="presentacion">
+				<h1>¡Juntas podemos!</h1>
+				<div id="info">
+                  <div class="seis cols margeni15">
+                  <p>Estos son algunos materiales gráficos producidos por distinta gente. Es una manera de ponerse en marcha, de tomar impulso, comenzando por lo que tienes mas a mano.</p></div>
+                  <div class="seis cols margeni15">
+                  <p>Este es el espíritu de la acción. Ponerse manos a la obra, cada cual como pueda. Lo único imprescindible es <strong>¡tener ganas de darle un Toque a Bankia!</strong></p></div>
+                <div class="cortar"></div>
+               </div><!--fin info-->
+            </div><!--fin presentación-->
+            <div id="listaYoutube">
+              <iframe width="853" height="480" src="http://www.youtube.com/embed/4r4eIxHiGWs?list=PLSKGMMincPZrJBefYYrrATgS6LJoVBGlW" frameborder="0" allowfullscreen></iframe>
+            </div>
+            
+            <div id="galeriaFotos">
+              <embed type="application/x-shockwave-flash" src="https://picasaweb.google.com/s/c/bin/slideshow.swf" width="853" height="568" flashvars="host=picasaweb.google.com&hl=es&feat=flashalbum&RGB=0x000000&feed=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2Ffeed%2Fapi%2Fuser%2F107760492625704995635%2Falbumid%2F5859634032433095553%3Falt%3Drss%26kind%3Dphoto%26hl%3Des" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>
+            </div>
+
+            
+            <div class="cortar"></div>
+            
+        
+        			
+	</div><!--Fin Principal-->
+    
+    <div id="pie"> </div>
+    
+   
+    
+	</body>
+</html>
