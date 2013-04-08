03f4cf597f2dc95d99ddb6795104eeed38cfcaea
Iván Eixarch <miao.org@gmail.com>
2013-04-08 18:01:01 +0200

    añadido piwik tracker

--- src/toque.php
+++ src/toque.php
@@ -63,20 +63,8 @@
 				</div>	
 			</div>
 	</div><!--Fin Principal-->
-    <div id="pie">
-		<div class="seis cols derecha entremete">
-			<a id="fb" href="https://www.facebook.com/Toque.a.Bankia"><img src="images/ballfacebook.png" /></a>
-			<div class="contadores">	
-				<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.toqueabankia.net&amp;send=false&amp;layout=button_count&amp;width=120&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:21px;" allowTransparency="true"></iframe>
-			</div>
-		</div>
-		<div class="seis cols izquierda entremete">
-			<a id="tw" href="https://twitter.com/ToqueaBankia"><img src="images/balltwitter.png" /></a>
-			<div class="contadores">
-				<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.toqueabankia.net" data-lang="es" data-related="ToqueaBankia" data-hashtags="toqueabankia">Twittear</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-			</div>
-		</div>	
-	</div>
+
+<?php include('pie.php'); ?>
+
 	</body>
 </html>
