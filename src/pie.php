f54bd299452801063a721c652e2c2fb7831093b8
Iván Eixarch <miao.org@gmail.com>
2013-04-08 18:01:25 +0200

    añadido pie

---
+++ src/pie.php
@@ -0,0 +1,32 @@
+    <div id="pie">
+		<div class="seis cols derecha entremete">
+			<a id="fb" href="https://www.facebook.com/Toque.a.Bankia"><img src="images/ballfacebook.png" /></a>
+			<div class="contadores">	
+				<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.toqueabankia.net&amp;send=false&amp;layout=button_count&amp;width=120&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:21px;" allowTransparency="true"></iframe>
+			</div>
+		</div>
+		<div class="seis cols izquierda entremete">
+			<a id="tw" href="https://twitter.com/ToqueaBankia"><img src="images/balltwitter.png" /></a>
+			<div class="contadores">
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.toqueabankia.net" data-lang="es" data-related="ToqueaBankia" data-hashtags="toqueabankia">Twittear</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			</div>
+		</div>	
+	</div>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.democraciarealya.es/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "49"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
