c291d3c0163387069b9a4c21bdea1952a0e6c94c
Luis Cabrera Sauco <lcabrera@sauco.org>
2013-04-10 13:36:33 +0100

    Desactivando la carga de fuentes mediante ajax.wq

--- src/index.php
+++ src/index.php
@@ -99,18 +99,6 @@
 					}
 				});
 
-				WebFontConfig = {
-					google: { families: [ 'Ubuntu::latin', 'Exo::latin' ] }
-				};
-				(function() {
-					var wf = document.createElement('script');
-					wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-					wf.type = 'text/javascript';
-					wf.async = 'true';
-					var s = document.getElementsByTagName('script')[0];
-					s.parentNode.insertBefore(wf, s);
-				})();
-
 			});
 
 		</script>
