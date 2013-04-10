75185ffc8e3a8d1b06d513bef3053fc8183314a2
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-04-10 14:44:38 +0100

    Cambios para evitar la carga de fuentes locales.

--- src/cabecera.php
+++ src/cabecera.php
@@ -3,10 +3,9 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta http-equiv="Content-Language" content="es" />
-		<meta name="author" content="Brankia S.A." />
-		<meta name="copyright" content="Brankia S.A., 2012. Rato. Todos los derechos en los reservados." />
+		<meta name="author" content="Rato, copyright" content="Brankia S.A., 2012. Rato. Todos los derechos en los reservados." />
 		<meta name="title" content="Brankia" />
-		<meta name="generator" content="Brankia S.A." />
+		<meta name="generator" content="Currantes de Brankia" />
 		<meta name="distribution" content="global" />
 		<meta name="revisit" content="0 day" />
 		<meta name="robots" content="all" />
@@ -21,8 +20,8 @@
 		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
 		<link href="estilos.css" type="text/css" rel="stylesheet" media="screen, projection" />
-		<link type="text/css" rel="stylesheet"  href="http://fonts.googleapis.com/css?family=Ubuntu" />
-		<link type="text/css" rel="stylesheet"  href="http://fonts.googleapis.com/css?family=Exo&effect=fire-animation" />
+		<!-- <link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' type='text/css' rel='stylesheet' /> -->
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu' type='text/css' rel='stylesheet' />
 
 		<title>Bankia - Toque a Bankia</title>
 	</head>
